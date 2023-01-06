956f72bc80a71e61a5292c35522d8f7f92e8f89a
juliaannem <117132281+juliaannem@users.noreply.github.com>
2023-01-05 20:07:07 -0500

    Added 6 day forecast

--- src/index.js
+++ src/index.js
@@ -32,6 +32,14 @@ function formatDate(now) {
 let dateTime = document.querySelector("#date-time");
 dateTime.innerHTML = formatDate(new Date());
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 //////////////////////////////////////////////////////////
 
 function handleSubmit(event) {
@@ -46,12 +54,11 @@ function search(city) {
   let apiKey = "d301fateao124ea76ce48beb7c353c42";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiURL).then(displayTemperature);
-  console.log(apiURL);
 }
 
 function displayTemperature(response) {
   console.log(response.data);
-  console.log(Math.round(response.data.wind.speed));
+  console.log(response.data.city);
 
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.temperature.humidity;
@@ -75,10 +82,52 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
 
-  let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.time * 1000);
-
   getForecast(response.data.city);
+}
+
+function getForecast(city) {
+  let apiKey = "d301fateao124ea76ce48beb7c353c42";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+  console.log(response.data.daily);
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+    
+        <img
+          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+            forecastDay.condition.icon
+          }.png"
+          alt=""
+          width="50"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-temperature-max"> ${Math.round(
+            forecastDay.temperature.maximum
+          )}° </span>
+          <span class="weather-temperature-min"> ${Math.round(
+            forecastDay.temperature.minimum
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 search("Vancouver");
